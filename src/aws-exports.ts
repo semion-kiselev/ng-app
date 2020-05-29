dad9fd12227a5378c7e8a70a50b1661b86334ae1
Siamion Kisialiou <siamion_kisialiou@epam.com>
2020-05-29 11:47:02 +0300

    fix build issue

---
+++ src/aws-exports.ts
@@ -0,0 +1,9 @@
+const awsmobile = {
+  aws_project_region: 'eu-central-1',
+  aws_cognito_region: 'eu-central-1',
+  aws_user_pools_id: 'eu-central-1_qA77VaXdf',
+  aws_user_pools_web_client_id: '5onak7t8k0cavmkv67q3timjku',
+  oauth: {}
+};
+
+export default awsmobile;
